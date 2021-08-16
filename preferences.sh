786d7b1468c04bd841a3929e5466a0a8bcec11a6
PJ Hurley <pwhurley@gmail.com>
2021-08-16 10:31:07 -0600

    added some hashtags to make the script a little more readable

--- preferences.sh
+++ preferences.sh
@@ -6,8 +6,9 @@ sudo -v
 # keep-alive: update existing `sudo` time stamp until we have finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-
+############
 ### DOCK ###
+############
 
 # automatically hide and show the dock
 defaults -currentHost write com.apple.dock autohide -bool true
@@ -15,7 +16,9 @@ defaults -currentHost write com.apple.dock autohide -bool true
 # don't show recent applications in dock
 defaults write com.apple.dock show-recents -bool false
 
+##############
 ### FINDER ###
+##############
 
 # show icons for external hard drives and removable media on the desktop
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
@@ -23,7 +26,9 @@ defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
+################
 ### TRACKPAD ###
+################
 
 # enable tap to click
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
@@ -37,7 +42,9 @@ defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool t
 # enfore natural scrolling
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true
 
+###############
 ### MENUBAR ###
+###############
 
 # show volume in menu bar
 defaults write com.apple.systemuiserver menuExtras -array-add "/System/Library/CoreServices/Menu Extras/Volume.menu"
@@ -54,7 +61,9 @@ defaults write com.apple.menuextra.clock DateFormat "EEE MMM d  h:mm:ss a"
 # reveal ip address, hostname, OS version, etc. when clicking the clock in the login window"
 sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
 
+###################
 ### MISC SYSTEM ###
+###################
 
 # enable the automatic update check
 defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
@@ -65,5 +74,9 @@ defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 # disable hibernation
 sudo pmset -a hibernatemode 0
 
-# kill affected applications
+###############
+### THE END ###
+###############
+
+# kill affected applications and log out
 osascript -e 'tell application "loginwindow" to  «event aevtrlgo»'
