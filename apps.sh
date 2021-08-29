7601433c3fdf8e9afbc788d3b7736598c932bcc2
PJ Hurley <pwhurley@gmail.com>
2021-08-29 17:11:48 -0600

    added apps downloads

---
+++ apps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# make this mess in the downloads directory
+cd ~/Downloads/
+
+# troubleshooting
+pwd
+
+# google chrome
+
+curl -O -J -L https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
+
+# visual studio code
+curl -O -J -L  https://go.microsoft.com/fwlink/?linkid=620882
+
+# iterm2
+curl -L https://iterm2.com/downloads/stable/latest --output iTerm2.zip
+
+# slack
+curl -L https://slack.com/ssb/download-osx --output Slack.dmg
