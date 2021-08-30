aa96e39369e4a86646005b72e683dbb0d87dc0d1
PJ Hurley <pwhurley@gmail.com>
2021-08-30 12:59:59 -0600

    added rectangle

--- apps.sh
+++ apps.sh
@@ -20,3 +20,6 @@ curl -L https://slack.com/ssb/download-osx --output Slack.dmg
 
 # zoom
 curl -O -L https://zoom.us/client/latest/ZoomInstaller.pkg
+
+# rectangle
+curl -O -L https://github.com/rxhanson/Rectangle/releases/download/v0.48/Rectangle0.48.dmg
