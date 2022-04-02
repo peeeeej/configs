beb132e8762e43285350427b593060ea88448fc5
PJ Hurley <pwhurley@gmail.com>
2022-04-02 09:32:13 -0600

    working on configuring vscode a bit

---
+++ code-extensions.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# need to follow the directions at 
+# https://code.visualstudio.com/docs/setup/mac#_launching-from-the-command-line first
+
+
+declare -a extensions=(
+    'ms-azuretools.vscode-docker' \
+    'hashicorp.terraform' \
+    'ms-python.vscode-pylance' \
+    'ms-python.python' \
+    'mechatroner.rainbow-csv' \
+    'formulahendry.terminal'
+    )
+
+for val in ${extensions[@]}; do
+    code --install-extension $val
+done
