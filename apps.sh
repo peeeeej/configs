f3f03e1587fa121b0dc781f11e13c54517f4ea5f
PJ Hurley <pwhurley@gmail.com>
2022-09-09 10:15:22 -0600

    updated rectangle to v 59

--- apps.sh
+++ apps.sh
@@ -36,7 +36,7 @@ curl -O -J -L  https://go.microsoft.com/fwlink/?linkid=2156837
 curl -L https://iterm2.com/downloads/stable/latest --output iTerm2.zip
 
 # rectangle
-curl -O -L https://github.com/rxhanson/Rectangle/releases/download/v0.58/Rectangle0.58.dmg
+curl -O -L https://github.com/rxhanson/Rectangle/releases/download/v0.59/Rectangle0.59.dmg
 
 # 1password
 curl -L  https://app-updates.agilebits.com/download/OPM7 --output 1Password.pkg
@@ -68,9 +68,9 @@ done
 ### .DMG FILES ###
 ##################
 
-declare -a dmgs=('googlechrome.dmg' 'Slack.dmg' 'Rectangle0.58.dmg')
+declare -a dmgs=('googlechrome.dmg' 'Slack.dmg' 'Rectangle0.59.dmg')
 
-declare -a volumes=('Google Chrome' 'Slack' 'Rectangle0.58')
+declare -a volumes=('Google Chrome' 'Slack' 'Rectangle0.59')
 
 # mount .dmg files
 for val in ${dmgs[@]}; do
@@ -80,7 +80,7 @@ done
 # copy apps to applications folder
 cp -R /Volumes/Google\ Chrome/Google\ Chrome.app /Applications
 cp -R /Volumes/Slack/Slack.app /Applications
-cp -R /Volumes/Rectangle0.58/Rectangle.app /Applications
+cp -R /Volumes/Rectangle0.59/Rectangle.app /Applications
 
 # eject .dmg files
 for val in "${volumes[@]}"; do
