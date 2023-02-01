74343ff634e8a159b5f7af7fd2cbcf3f258301a1
PJ Hurley <pwhurley@gmail.com>
2023-02-01 14:21:56 -0700

    changed code extension script to use installed bash version, also updated the extensions list

--- code-extensions.sh
+++ code-extensions.sh
@@ -1,18 +1,16 @@
-#!/bin/zsh
+#!/usr/bin/env bash
 
 # need to follow the directions at 
 # https://code.visualstudio.com/docs/setup/mac#_launching-from-the-command-line first
 
 
 declare -a extensions=(
-    'ms-azuretools.vscode-docker' \
-    'hashicorp.terraform' \
     'ms-python.vscode-pylance' \
     'ms-python.python' \
     'mechatroner.rainbow-csv' \
     'bmalehorn.shell-syntax'
     )
 
-for val in ${extensions[@]}; do
-    code --install-extension $val
+for val in "${extensions[@]}"; do
+    code --install-extension "$val"
 done
